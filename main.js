70e9ffaae6954ac19e9f836b7c9052dc386069ad
Patrik930 <focimc02@gmail.com>
2024-04-03 14:21:00 +0200

    cards

--- main.js
+++ main.js
@@ -1,24 +1,48 @@
 import { getData } from "./getData.js"
 import { fetchMotorcycles } from "./fetch-moto-api.js";
 
-const project = ()=>{
-    console.log("ok");
-      const url = "https://raw.githubusercontent.com/Patrik930/MotoProjektImages/main/MotoImages"
-      getData(url,renderData)
-      let row = document.querySelector(".sor")
-      function renderData(data){
-          console.log(data);
-          data.forEach(e => {
-          row.innerHTML += `<div class="max-w-sm rounded overflow-hidden shadow-lg">
-          <img class="w-full" src="${e.img}">
-          <div class="px-6 py-4">
-            <div class="font-bold text-xl mb-2 center">${e.model}</div>
-            <p class="text-gray-700 text-base"></p>
-          </div>
-      </div>`
+let images = []
 
-        
-          })
-    }
+
+const project = ()=>{
+      const url = "https://raw.githubusercontent.com/Patrik930/MotoProjektImages/main/MotoImages"
+      getData(url,renderImages)
+      
 }
+
+
+function renderImages(data){
+  /* console.log(data);
+  */
+   images = data
+   console.log(images);
+   fetchMotorcycles(renderData)
+   /*
+   */
+}
+
+
+
 project()
+
+function renderData(data){
+  console.log(data);
+  console.log(images);
+
+  let row = document.querySelector(".sor")
+  data.forEach(e => {
+    let imgObj = images.find(obj => obj.model.trim().includes(e.model.trim()))
+    if(imgObj){
+
+    row.innerHTML += `<div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img class="w-full aspect-square object-contain" src="${imgObj?.img}" alt="${e.model}">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2 text-center text-lime-700">${e.model}</div>
+      <button class="myBtn">More Info</button>
+      <p class="text-gray-700 text-base"></p>
+    </div>
+ </div>`
+    }  
+    })
+}
+
